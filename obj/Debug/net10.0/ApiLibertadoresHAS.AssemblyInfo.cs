49fb1b35897f3702b40f5797558b32d906673ee3
aaurichio <matheusaurichiobaptista8@gmail.com>
2026-08-24 11:50:56 -0300

    24/08

--- obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2edc493f4922ca1546826cfe4c642530d683fbb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e65f91bff0c3b7c8171e5cb889da7e50ee11dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
